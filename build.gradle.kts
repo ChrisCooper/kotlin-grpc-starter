ecb6ed65bfd9b808eb495c8aa0e2051d8c2ba91b
= <=>
2020-05-03 19:13:50 -0700

    simple proto generation working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
+
 plugins {
     java
     kotlin("jvm") version "1.4-M1"
+    idea
     id("com.google.protobuf") version "0.8.8"
 }
 
@@ -26,6 +30,7 @@ dependencies {
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
@@ -34,4 +39,29 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+}
+
+// Custom task to copy proto-generated java files from build/ to src/
+//tasks.register("syncGeneratedSources", Sync::class){
+//    from("$buildDir/generated/source/proto/main")
+//    into("$rootProject.projectDir/java/src")
+//}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.0.0"
+        // or
+        // path = 'tools/protoc'
+    }
+
+    //generatedFilesBaseDir = "$projectDir/gen"
+
+//    generateProtoTasks.ofSourceSet("main").forEach { task ->
+//    }
+}
+
+
+tasks.clean {
+    //delete(protobuf.generatedFilesBaseDir)
+    //delete(protobuf.)
 }
