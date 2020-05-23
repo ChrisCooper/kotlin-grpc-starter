047e4429954aa6a0b3799ef56acb0f6df88be6f9
= <=>
2020-05-23 01:20:27 -0700

    shitty WIP attemp at gradle grpc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,13 @@
-import com.google.protobuf.gradle.ofSourceSet
-import com.google.protobuf.gradle.protobuf
-import com.google.protobuf.gradle.protoc
+import com.google.protobuf.gradle.*
 
 plugins {
     java
     kotlin("jvm") version "1.4-M1"
     idea
-    id("com.google.protobuf") version "0.8.12"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "primtech"
+version = "1.0.0"
 
 val grpcVersion = "1.29.0"
 val protobufVersion = "3.11.0"
@@ -29,13 +26,12 @@ dependencies {
     implementation("io.grpc", "grpc-protobuf", grpcVersion)
     implementation("io.grpc", "grpc-stub", grpcVersion)
 
-    testCompile("junit", "junit", "4.12")
+    testImplementation("junit", "junit", "4.12")
 
     compileOnly("org.apache.tomcat", "annotations-api", "6.0.53")
 
     runtimeOnly("io.grpc", "grpc-netty-shaded", grpcVersion)
 }
-
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
@@ -50,48 +46,45 @@ tasks {
     }
 }
 
+protobuf {
+    protoc { artifact = "com.google.protobuf:protoc:${protocVersion}" }
+
+    generateProtoTasks.ofSourceSet("main").forEach{ task ->
+        plugins {
+        }
+        println("TASKKK: ${task}")
+    }
+
+    plugins {
+
+    }
+
+//    plugins {
+//        generateProtoTasks.ofSourceSet("main").forEach { task ->
+//            println(task)
+//            // grpc { artifact = "io.grpc:protoc-gen-grpc-java:${grpcVersion}" }
+//        }
+//    }
+}
+
+// Inform IDEs like IntelliJ IDEA, Eclipse or NetBeans about the generated code.
+//sourceSets {
+//    main {
+//        java {
+//            srcDirs("build/generated/source/proto/main/grpc")
+//            srcDirs("build/generated/source/proto/main/java")
+//        }
+//    }
+//}
+
+//tasks.clean {
+    //delete(protobuf.generatedFilesBaseDir)
+    //delete(protobuf.)
+//}
+
+
 // Custom task to copy proto-generated java files from build/ to src/
 //tasks.register("syncGeneratedSources", Sync::class){
 //    from("$buildDir/generated/source/proto/main")
 //    into("$rootProject.projectDir/java/src")
 //}
-
-protobuf {
-    protoc { artifact = "com.google.protobuf:protoc:${protocVersion}" }
-
-//    plugins {
-//        grpc { artifact = "io.grpc:protoc-gen-grpc-java:${grpcVersion}" }
-//    }
-//
-//    generateProtoTasks {
-//        all()*.plugins { grpc {} }
-//    }
-
-//    generateProtoTasks.ofSourceSet("main").forEach { task ->
-//        task.plugins {
-//        }
-//    }
-
-    //plugins {
-    //}
-
-    //generatedFilesBaseDir = "$projectDir/gen"
-
-//    generateProtoTasks.ofSourceSet("main").forEach { task ->
-//    }
-}
-
-// Inform IDEs like IntelliJ IDEA, Eclipse or NetBeans about the generated code.
-sourceSets {
-    main {
-        java {
-            srcDirs("build/generated/source/proto/main/grpc")
-            srcDirs("build/generated/source/proto/main/java")
-        }
-    }
-}
-
-tasks.clean {
-    //delete(protobuf.generatedFilesBaseDir)
-    //delete(protobuf.)
-}
